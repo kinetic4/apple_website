271d003825a19c6e9ebea5624822bfa2e8f2adc3
kinetic4 <pratyushbhargava6@gmail.com>
2024-11-28 00:03:53 +0530

    FIX DEPLOYMENT

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0'
+  }
 })
 
