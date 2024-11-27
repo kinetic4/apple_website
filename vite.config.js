c2ec4b7c2b6e6c865699c4ac8ee4049d01b2c6d4
kinetic4 <pratyushbhargava6@gmail.com>
2024-11-28 00:09:56 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: process.env.PORT || 4173,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
     host: '0.0.0.0'
   }
-})
-
+});
